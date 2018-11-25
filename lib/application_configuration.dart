e65b51415c6dc1c65f27a634613626c0a6a1a067
Feroz Ahmmed <froghramar@gmail.com>
2018-11-25 12:45:41 +0600

    add  package safe config

---
+++ lib/application_configuration.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+import 'package:safe_config/safe_config.dart';
+
+class ApplicationConfiguration extends Configuration {
+	ApplicationConfiguration(String fileName) :
+			super.fromFile(File(fileName));
+
+	Map<String, String> database;
+}
--- lib/database.dart
+++ lib/database.dart
@@ -1,9 +1,13 @@
 import 'package:mongo_dart/mongo_dart.dart';
+import 'application_configuration.dart';
 
 class Database {
 
 	static Future<DbCollection> collection(String collectionName) {
-		final Db database = Db('mongodb://localhost:27017/schooldb');
+
+		final config = ApplicationConfiguration('config.yaml');
+
+		final Db database = Db(config.database["connectionString"]);
 		return database.open().then((d) {
 				return database.collection(collectionName);
 			}
